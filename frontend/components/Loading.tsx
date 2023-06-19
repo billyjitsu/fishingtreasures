3a9c470553c3e198599cb6680582a9f0399d1642
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-06-18 20:53:22 -0700

    update loading screen

--- frontend/components/Loading.tsx
+++ frontend/components/Loading.tsx
@@ -1,18 +1,20 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Image from "next/image";
-import fishing from "../images/fishing.webp"
+import fishing from "../images/fishing.webp";
+import heroImage from "../images/igloo-redo-dark.webp";
 
 const Loading = () => {
   return (
-    <div className= "flex flex-col text-white items-center justify-center mb-10 md:mb-0 h-screen w-full bg-slate-100">
-        <Image src={fishing}
-        alt="fishing"
-        width={400}
-        height={400}/>
-        
-      <h2 className="flex flex-col text-black text-center justify-center text-3xl font-bold animate-pulse mb-10 md:mb-0 w-full">
-        Fishing...
-      </h2>
+    <div className="relative flex flex-col text-white items-center justify-center mb-10 md:mb-0 h-screen w-full">
+      <Image src={heroImage} alt="heroBanner" fill priority />
+      <div className="absolute flex flex-col items-center justify-center">
+        <Image src={fishing} alt="fishing" width={300} height={300} />
+
+        <h2 className="text-white text-center justify-center text-3xl font-bold animate-pulse mb-10 md:mb-0 w-full drop-shadow-[0_2.2px_1.2px_rgba(0,0,0,0.8)]">
+          Let's go fishing...
+        </h2>
+      </div>
     </div>
   );
 };
